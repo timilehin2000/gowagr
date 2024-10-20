b86b9b98f94491b81b54101e0decc363ba558b34
Ayomiwole <ayomidemajewole@gmail.com>
2024-10-20 18:28:35 +0100

    Develop/Feat: Define Transfer Query Interface

---
+++ src/transfer/transfer.interface.ts
@@ -0,0 +1,8 @@
+export interface TransferQueryConditions {
+  otherPartyUsername?: string;
+  date?: string;
+  minimumAmount?: number;
+  maximumAmount?: number;
+  startDate?: string;
+  endDate?: string;
+}
